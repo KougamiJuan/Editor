227c408b126cae1930872b5413169270fb3d22cb
Juan Pablo <ujuanpablo@gmail.com>
2019-06-24 22:43:10 -0500

    Creacion Editor de texto basico

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 24 22:24:49 2019
+
+@author: Juan Pablo Umbarila Granada
+"""
+
+from setuptools import setup
+ 
+setup(name="Editor",
+      version="0.1",
+      description="Editor de Texto Basico",
+      author="Juan Pablo Umbarila Granada",
+      author_email='ujuanpablo@gmail.com',
+      license="GPL",
+      packages=['dist'],
+)
